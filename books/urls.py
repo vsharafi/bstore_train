9d8afd7f111ec5100669336038f5d538ef5e2ef5
vahid <vahid.sh1431@yahoo.com>
2024-02-04 22:52:38 +0330

    added book list detail create and update

--- books/urls.py
+++ books/urls.py
@@ -5,4 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.BookListView.as_view(), name='home'),
+    path('<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
+    path('create/', views.BookCreateView.as_view(), name='book_create'),
+    path('<int:pk>/update/', views.BookUpdateView.as_view(), name='book_update'),
+    
 ]
--- books/views.py
+++ books/views.py
@@ -1,12 +1,37 @@
-from typing import Any
-from django.db.models.query import QuerySet
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.views import generic
 from .models import Book
-
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
 
 class BookListView(generic.ListView):
     template_name = 'books/book_list.html'
     model = Book
     context_object_name = 'books'
-    def get_queryset(self):
-        return Book.objects.all().order_by('title')
+    # def get_queryset(self):
+    #     return Book.objects.all().order_by('title')
+    
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'books/book_detail.html'
+
+
+class BookCreateView(generic.CreateView):
+    model = Book
+    template_name = 'books/book_create.html'
+    fields = '__all__'
+
+
+class BookUpdateView(generic.UpdateView):
+    model = Book
+    template_name = 'books/book_update.html'
+    fields = '__all__'
+
+    def form_valid(self, form):
+        if form.has_changed():
+            form.save()
+            return redirect('form_detail', pk=self.object.pk)
+        else:
+            return redirect('book_update', pk=self.object.pk)
