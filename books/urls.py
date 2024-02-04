b0ff4539b261985ff3721152ae7ee9ca7bbb1d5f
vahid <vahid.sh1431@yahoo.com>
2024-02-04 19:32:00 +0330

    add book list view

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from . import views
 
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='book_list.html'), name='home'),
+    path('', views.BookListView.as_view(), name='home'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from typing import Any
+from django.db.models.query import QuerySet
+from django.views import generic
+from .models import Book
 
-# Create your views here.
+
+class BookListView(generic.ListView):
+    template_name = 'books/book_list.html'
+    model = Book
+    context_object_name = 'books'
+    def get_queryset(self):
+        return Book.objects.all().order_by('title')
